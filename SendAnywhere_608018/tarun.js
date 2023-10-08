e90cf37010df05380a9dad42e76316b83477992d
Karthikeya Kilaru <karthikeyakilaru@Karthikeyas-MBP.bbrouter>
2023-10-08 14:58:04 +0530

    first commit

---
+++ SendAnywhere_608018/tarun.js
@@ -0,0 +1,56 @@
+let products={
+    data:{
+    {
+        {
+            RecipesName:"Murg Makhani",
+            Category:"Non-Veg",
+            Image:"murg makhani.jpg",
+        },
+        {
+            RecipesName:"Nihari Gosht",
+            Category:"Non-Veg",
+            Image:"nihari gosht.jpg",
+        },
+        {
+            RecipesName:"Chole Bhature",
+            Category:"Veg",
+            Image:"chole bhature.jpg",
+        },
+        {
+            RecipesName:"Paneer Paratha",
+            Category:"Veg"
+            Image:"paneer paratha.jpg",
+        },
+        {
+            RecipesName:"Zunka Bhakri",
+            Category:"Veg",
+            Image:"zunka bhakri.jpg",
+        },
+        {
+            RecipesName:"Karanji",
+            Category:"Dessert",
+            Image:"karanji.jpg",
+        },
+        {
+            RecipesName:"Shrikhand",
+            Category:"Dessert",
+            Image:"shrikhand.jpg"
+        },
+        {
+            RecipesName:"Bebinca",
+            Category:"Dessert",
+            Image:"bebinca.jpg",
+        },
+        {
+            RecipesName:"Machcher Jhol",
+            Category:"Non-Veg",
+            Image:"machcher jhol.jpg",
+        },
+        {
+            RecipesName:"Hyderabadi Chicken Biryani",
+            Category:"Non-Veg",
+            Image:"hyderabadi chicken biryani.jpg",
+        },
+    }
+    }
+}
